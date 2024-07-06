1cc4d9f1785f70d308eaa1e8870d0fec971c6c46
KKardd <wlstjd4013@gmail.com>
2024-07-07 01:19:08 +0900

    feat: apply typeorm in user module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { UsersReposiotory } from './users.repository';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity-typeorm/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UsersController],
   providers: [UsersService, UsersReposiotory],
 })
