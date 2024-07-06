f2e596cc5a4e39457ed42fdd8ec047850f603256
KKardd <wlstjd4013@gmail.com>
2024-07-07 01:16:50 +0900

    feat: add find-all-user-by-filtering controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { FilterUserOption } from './enum/filter-user.enum';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersServcie: UsersService) {}
+
+  async findAllUsersByFiltering(@Query('filter') type: FilterUserOption) {
+    return await this.usersServcie.findAllUsersByFiltering(type);
+  }
 }
