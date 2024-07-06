bd34bb4e1c64a9063b6f1d1ac452643d648305d0
KKardd <wlstjd4013@gmail.com>
2024-07-06 21:16:58 +0900

    feat: add users-repository layer

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { UsersReposiotory } from './users.repository';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService, UsersReposiotory],
 })
 export class UsersModule {}
