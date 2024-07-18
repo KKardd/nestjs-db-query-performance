85d59801bdde67a402bd855fdea850775ec7d31b
KKardd <wlstjd4013@gmail.com>
2024-07-18 16:23:46 +0900

    feat: apply orms in repository layer

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Users } from './entity-typeorm/users.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersReposiotory {}
+export class UsersReposiotory {
+  constructor(
+    @InjectRepository(Users)
+    private readonly users: Repository<Users>,
+  ) {}
+}
