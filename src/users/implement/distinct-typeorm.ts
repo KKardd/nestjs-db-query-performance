bc414c790015e3299da22faca14aed672f7af25d
KKardd <wlstjd4013@gmail.com>
2024-07-21 23:32:09 +0900

    feat: apply find query in distinct-typeorm

--- src/users/implement/distinct-typeorm.ts
+++ src/users/implement/distinct-typeorm.ts
@@ -4,4 +4,8 @@ import { UsersReposiotory } from '../users.repository';
 @Injectable()
 export class DistinctTypeORM {
   constructor(private readonly usersRepository: UsersReposiotory) {}
+
+  async findTypeORM() {
+    return await this.usersRepository.findDistinctUsersForTypeORM();
+  }
 }
