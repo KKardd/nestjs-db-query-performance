f4bfafaf8a00eb68f12e2796ce5a17f5ca2080bf
KKardd <wlstjd4013@gmail.com>
2024-07-21 23:07:15 +0900

    feat: add find query in repository

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,12 +1,66 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from './entity-typeorm/users.entity';
-import { Repository } from 'typeorm';
+import { EntityManager, Repository } from 'typeorm';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class UsersReposiotory {
   constructor(
     @InjectRepository(Users)
     private readonly users: Repository<Users>,
+    private readonly entityManager: EntityManager,
+    private readonly prisma: PrismaClient,
   ) {}
+
+  // 조건없는 모든 유저 찾기(TypeORM)
+  async findUsersForTypeORM() {
+    return await this.users.find({
+      relations: {
+        company: true,
+      },
+    });
+  }
+
+  // 정렬(companyId)되어 있는 모든 유저 찾기(TypeORM)
+  async findSortedUsersForTypeORM() {
+    return await this.users.find({
+      order: {
+        companyId: 'ASC',
+      },
+    });
+  }
+
+  // 중복제거(companyId)한 모든 유저 찾기(TypeORM)
+  async findDistinctUsersForTypeORM() {
+    return await this.entityManager
+      .createQueryBuilder(Users, 'u')
+      .distinctOn(['u.company_id'])
+      .getRawMany();
+  }
+
+  // 조건없는 모든 유저 찾기(prisma)
+  async findUsersForPrisma() {
+    return await this.prisma.users.findMany({
+      include: {
+        company: true,
+      },
+    });
+  }
+
+  // 정렬(companyId)되어 있는 모든 유저 찾기(prisma)
+  async findSortedUsersForPrisma() {
+    return await this.prisma.users.findMany({
+      orderBy: {
+        name: 'asc',
+      },
+    });
+  }
+
+  // 중복제거(companyId)한 모든 유저 찾기(prisma)
+  async findDistinctUsersForPrisma() {
+    return await this.prisma.users.findMany({
+      distinct: ['companyId'],
+    });
+  }
 }
