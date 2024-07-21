78ec884a009e71f9e202453c40db06964917ce34
KKardd <wlstjd4013@gmail.com>
2024-07-22 06:51:50 +0900

    feat: apply find query in sort-prisma

--- src/users/implement/sort-prisma.ts
+++ src/users/implement/sort-prisma.ts
@@ -4,4 +4,8 @@ import { UsersReposiotory } from '../users.repository';
 @Injectable()
 export class SortPrisma {
   constructor(private readonly usersRepository: UsersReposiotory) {}
+
+  async findPrisma() {
+    return await this.usersRepository.findSortedUsersForPrisma();
+  }
 }
