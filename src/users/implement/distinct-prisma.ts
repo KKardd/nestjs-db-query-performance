6e1fd6da4388ef74cc6cee8efb442bee3e4c2623
KKardd <wlstjd4013@gmail.com>
2024-07-21 23:31:47 +0900

    feat: apply find query in distinct-prisma

--- src/users/implement/distinct-prisma.ts
+++ src/users/implement/distinct-prisma.ts
@@ -4,4 +4,8 @@ import { UsersReposiotory } from '../users.repository';
 @Injectable()
 export class DistinctPrisma {
   constructor(private readonly usersRepository: UsersReposiotory) {}
+
+  async findPrisma() {
+    return await this.usersRepository.findDistinctUsersForPrisma();
+  }
 }
