5891dcfde2685f14a500abc4d92eaf4282a1dba1
KKardd <wlstjd4013@gmail.com>
2024-09-01 22:30:42 +0900

    feat: add sort by name method

--- src/users/implement/sort-server.ts
+++ src/users/implement/sort-server.ts
@@ -4,4 +4,22 @@ import { UsersReposiotory } from '../users.repository';
 @Injectable()
 export class SortServer {
   constructor(private readonly usersRepository: UsersReposiotory) {}
+
+  async findPrisma() {
+    const data = await this.usersRepository.findUsersForPrisma();
+    return this.sortDataByName(data);
+  }
+
+  async findTypeORM() {
+    const data = await this.usersRepository.findUsersForTypeORM();
+    return this.sortDataByName(data);
+  }
+
+  private sortDataByName(data) {
+    return data.sort((a, b) => {
+      if (a.name < b.name) return -1;
+      if (a.name > b.name) return 1;
+      return 0;
+    });
+  }
 }
