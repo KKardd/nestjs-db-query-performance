ddc34581080740a85a10fbb0728179dccf554d6f
KKardd <wlstjd4013@gmail.com>
2024-09-11 23:47:18 +0900

    refactor: apply dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
 
 async function bootstrap() {
+  dotenv.config({
+    path: path.resolve('./src/.env'),
+  });
+
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
