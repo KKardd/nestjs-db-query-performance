545af14ff8f6201eaea645eb7e5d02ea1d1dc7ed
KKardd <wlstjd4013@gmail.com>
2024-09-13 22:45:33 +0900

    fix: insert-company script about mysql2

--- src/data/insert-company.js
+++ src/data/insert-company.js
@@ -1,14 +1,14 @@
 const fs = require('fs');
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const csv = require('csv-parser');
 const config = require('./database-config.constant');
 
 const URL = config();
 // MySQL 연결 정보 설정
-const connection = mysql.createConnection(URL);
+let connection;
 
 // CSV 파일 경로
-const csvFilePath = process.cwd() + '/data/FINAL_COMPANY_INFO_filter.csv';
+const csvFilePath = process.cwd() + '/src/data/FINAL_COMPANY_INFO_filter.csv';
 
 const bigJobKind = [];
 const midJobKind = [];
@@ -17,110 +17,99 @@ let bigJobIdx = 0;
 let midJobIdx = 0;
 
 // CSV 파일을 MySQL 테이블에 삽입하는 함수
-function insertCsvDataToMysql() {
-  // CSV 파일을 읽기 위한 스트림 생성
-  const stream = fs.createReadStream(csvFilePath).pipe(csv());
+async function insertCsvDataToMysql() {
+  try {
+    connection = await mysql.createConnection(URL);
+    console.log('Connected to MySQL.');
 
-  // 스트림에서 데이터를 읽을 때마다 실행되는 콜백 함수
-  stream.on('data', async (data) => {
-    // MySQL 테이블에 데이터를 삽입하는 쿼리 작성
+    // CSV 파일을 읽기 위한 스트림 생성
+    const stream = fs.createReadStream(csvFilePath).pipe(csv());
 
-    // 직업 대분류 넣기
-    if (!bigJobKind.includes(data.big_company_kind)) {
-      bigJobKind.push(data.big_company_kind);
-      bigJobIdx++;
+    const insertPromises = []; // 비동기 작업을 추적하기 위한 배열
 
-      const insertBigJobQuery = 'INSERT INTO big_job_kind (name) VALUES (?)';
-      await connection.query(
-        insertBigJobQuery,
-        [data.big_company_kind],
-        (error, results, fields) => {
-          if (error) {
-            console.error('An error occurred while inserting data:', error);
+    // 스트림에서 데이터를 읽을 때마다 실행되는 콜백 함수
+    stream.on('data', (data) => {
+      const insertData = async () => {
+        try {
+          // 직업 대분류 넣기
+          if (!bigJobKind.includes(data.big_company_kind)) {
+            bigJobKind.push(data.big_company_kind);
+            bigJobIdx++;
+
+            const insertBigJobQuery =
+              'INSERT INTO big_job_kind (name) VALUES (?)';
+            await connection.query(insertBigJobQuery, [data.big_company_kind]);
           }
-        },
-      );
-    }
 
-    // 직업 중분류 넣기
-    if (!midJobKind.includes(data.mid_company_kind)) {
-      midJobKind.push(data.mid_company_kind);
-      midJobIdx++;
+          // 직업 중분류 넣기
+          if (!midJobKind.includes(data.mid_company_kind)) {
+            midJobKind.push(data.mid_company_kind);
+            midJobIdx++;
 
-      const insertMidJobQuery =
-        'INSERT INTO mid_job_kind (big_job_kind_id, name) VALUES (?, ?)';
-      await connection.query(
-        insertMidJobQuery,
-        [bigJobIdx, data.mid_company_kind],
-        (error, results, fields) => {
-          if (error) {
-            console.error('An error occurred while inserting data:', error);
+            const insertMidJobQuery =
+              'INSERT INTO mid_job_kind (big_job_kind_id, name) VALUES (?, ?)';
+            await connection.query(insertMidJobQuery, [
+              bigJobIdx,
+              data.mid_company_kind,
+            ]);
           }
-        },
-      );
-    }
 
-    if (!smallJobKind.includes(data.company_kind)) {
-      smallJobKind.push(data.company_kind);
-      const insertSmallJobQuery =
-        'INSERT INTO small_job_kind (mid_job_kind_id, name) VALUES (?, ?)';
-      await connection.query(
-        insertSmallJobQuery,
-        [midJobIdx, data.company_kind],
-        (error, results, fields) => {
-          if (error) {
-            console.error('An error occurred while inserting data:', error);
+          // 직업 소분류 넣기
+          if (!smallJobKind.includes(data.company_kind)) {
+            smallJobKind.push(data.company_kind);
+            const insertSmallJobQuery =
+              'INSERT INTO small_job_kind (mid_job_kind_id, name) VALUES (?, ?)';
+            await connection.query(insertSmallJobQuery, [
+              midJobIdx,
+              data.company_kind,
+            ]);
           }
-        },
-      );
-    }
 
-    const bigJobKindId = bigJobKind.indexOf(data.big_company_kind) + 1;
-    const midJobKindId = midJobKind.indexOf(data.mid_company_kind) + 1;
-    const smallJobKindId = smallJobKind.indexOf(data.company_kind) + 1;
-    if (bigJobKindId === 0 || midJobKindId === 0 || smallJobKindId === 0) {
-      throw new Error('이상함');
-    }
+          const bigJobKindId = bigJobKind.indexOf(data.big_company_kind) + 1;
+          const midJobKindId = midJobKind.indexOf(data.mid_company_kind) + 1;
+          const smallJobKindId = smallJobKind.indexOf(data.company_kind) + 1;
 
-    const query =
-      'INSERT INTO company (name, address, longitude, latitude, scale, big_job_kind_id, mid_job_kind_id, small_job_kind_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-    // 쿼리 실행
+          if (
+            bigJobKindId === 0 ||
+            midJobKindId === 0 ||
+            smallJobKindId === 0
+          ) {
+            throw new Error('Invalid job kind');
+          }
 
-    await connection.query(
-      query,
-      [
-        data.company_name,
-        data.company_address,
-        data.longitude,
-        data.latitude,
-        data.company_scale,
-        bigJobKindId,
-        midJobKindId,
-        smallJobKindId,
-      ],
-      (error, results, fields) => {
-        if (error) {
-          console.error('An error occurred while inserting data:', error);
+          const query =
+            'INSERT INTO company (name, address, longitude, latitude, scale, big_job_kind_id, mid_job_kind_id, small_job_kind_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
+          await connection.query(query, [
+            data.company_name,
+            data.company_address,
+            data.longitude,
+            data.latitude,
+            data.company_scale,
+            bigJobKindId,
+            midJobKindId,
+            smallJobKindId,
+          ]);
+        } catch (error) {
+          console.error('An error occurred while processing data:', error);
         }
-      },
-    );
-  });
+      };
 
-  // 모든 데이터가 삽입된 후에 실행되는 콜백 함수
-  stream.on('end', () => {
-    console.log('All data has been inserted into MySQL.');
-    // MySQL 연결 종료
-    connection.end();
-  });
+      insertPromises.push(insertData()); // 비동기 작업을 배열에 추가
+    });
+
+    stream.on('end', async () => {
+      try {
+        // 모든 비동기 작업이 완료될 때까지 기다림
+        await Promise.all(insertPromises);
+        console.log('All data has been inserted into MySQL.');
+      } finally {
+        await connection.end(); // MySQL 연결 종료
+      }
+    });
+  } catch (err) {
+    console.error('Error connecting to MySQL:', err);
+  }
 }
 
-// MySQL 연결
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
-  }
-  console.log('Connected to MySQL.');
-  // CSV 데이터 삽입 시작
-  insertCsvDataToMysql();
-});
+// CSV 데이터 삽입 시작
+insertCsvDataToMysql();
